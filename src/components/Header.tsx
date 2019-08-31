6f6aa5e46bf7d6af40168bf19ea5cf122f9e3c8c
BravenxX <BravenxX@users.noreply.github.com>
2019-08-31 18:11:54 -0400

    primer commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export class Header extends React.Component {
+  public render() {
+    <header className="App-header">
+      <h1 className="App-title">Welcome to React</h1>
+    </header>;
+  }
+}
